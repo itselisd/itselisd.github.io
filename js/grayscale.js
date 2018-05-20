78d7bdefd6eb54db7eb844dd73de3a23f59faa32
Elis Dervishi <elis.derv@gmail.com>
2018-05-20 10:07:18 -0400

    minor changes and css tweaks

--- js/grayscale.js
+++ js/grayscale.js
@@ -46,7 +46,7 @@ var map = null;
 // When the window has finished loading create our google map below
 google.maps.event.addDomListener(window, 'load', init);
 google.maps.event.addDomListener(window, 'resize', function() {
-  map.setCenter(new google.maps.LatLng(40.9596, -73.8096));
+  map.setCenter(new google.maps.LatLng(40.7506, -73.9935));
 });
 
 function init() {
@@ -57,7 +57,7 @@ function init() {
     zoom: 15,
 
     // The latitude and longitude to center the map (always required)
-    center: new google.maps.LatLng(40.9596, -73.8096), // New York
+    center: new google.maps.LatLng(40.7506, -73.9935), // New York
 
     // Disables the default Google Maps UI components
     disableDefaultUI: true,
@@ -185,7 +185,7 @@ function init() {
 
   // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
   var image = 'img/map-marker.svg';
-  var myLatLng = new google.maps.LatLng(40.9596, -73.8096);
+  var myLatLng = new google.maps.LatLng(40.7506, -73.9935);
   var beachMarker = new google.maps.Marker({
     position: myLatLng,
     map: map,
